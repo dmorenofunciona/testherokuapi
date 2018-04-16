83aab195044ca2f66b1029d56b4ab1c3d8e357e0
dmorenofunciona <38423855+dmorenofunciona@users.noreply.github.com>
2018-04-16 12:03:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,20 +6,19 @@ const http = require('http');
 const API_KEY = require('./apiKey');
 
 const server = express();
+const level = 0;
+
 server.use(bodyParser.urlencoded({
     extended: true
 }));
 
 server.use(bodyParser.json());
 server.post('/get-movie-details', (req, res) => {
-    console.log(req.body);
-
+    console.log(req.body.result.resolvedQuery);
     var speech =
     req.body.result &&
     req.body.result.resolvedQuery &&
-    req.body.result.resolvedQuery.echoText
-      ? req.body.result.parameters.echoText
-      : "Seems like some problem. Speak again.";
+    req.body.result.resolvedQuery.echoText ? req.body.result.resolvedQuery.echoText : "Seems like some problem. Speak again.";
   return res.json({
     speech: speech,
     displayText: speech,
@@ -28,6 +27,6 @@ server.post('/get-movie-details', (req, res) => {
 });
 
 
-server.listen((process.env.PORT || 8888), () => {
+server.listen((process.env.PORT || 8000), () => {
     console.log("Server is up and running...");
 });
